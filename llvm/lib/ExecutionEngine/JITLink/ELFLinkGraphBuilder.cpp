2d2e9c8810000fd32a895a5a08bc4d402e538502
Sona <s.reddy@unsw.edu.au>
2025-01-23 18:19:59 +1100

    removed changes to ELFLingGraphBuilder.cpp

--- llvm/lib/ExecutionEngine/JITLink/ELFLinkGraphBuilder.cpp
+++ llvm/lib/ExecutionEngine/JITLink/ELFLinkGraphBuilder.cpp
@@ -20,7 +20,6 @@ static const char *DWSecNames[] = {
 namespace llvm {
 namespace jitlink {
 StringRef ELFLinkGraphBuilderBase::CommonSectionName(".common");
-StringRef ELFLinkGraphBuilderBase::OriginalObjectContentSectionName(".jitlink_original_object_content");
 ArrayRef<const char *> ELFLinkGraphBuilderBase::DwarfSectionNames = DWSecNames;
 
 ELFLinkGraphBuilderBase::~ELFLinkGraphBuilderBase() = default;
