c42fab51733f06f5a0a2f1e7a6d00189ec1d5a37
Sona <s.reddy@unsw.edu.au>
2025-01-23 18:19:59 +1100

    removed changes to ELFLingGraphBuilder.cpp

--- llvm/lib/ExecutionEngine/JITLink/ELFLinkGraphBuilder.cpp
+++ llvm/lib/ExecutionEngine/JITLink/ELFLinkGraphBuilder.cpp
@@ -9,23 +9,17 @@
 // Generic ELF LinkGraph building code.
 //
 //===----------------------------------------------------------------------===//
-
 #include "ELFLinkGraphBuilder.h"
-
 #define DEBUG_TYPE "jitlink"
-
 static const char *DWSecNames[] = {
 #define HANDLE_DWARF_SECTION(ENUM_NAME, ELF_NAME, CMDLINE_NAME, OPTION)        \
   ELF_NAME,
 #include "llvm/BinaryFormat/Dwarf.def"
 #undef HANDLE_DWARF_SECTION
 };
-
 namespace llvm {
 namespace jitlink {
-
 StringRef ELFLinkGraphBuilderBase::CommonSectionName(".common");
-StringRef ELFLinkGraphBuilderBase::OriginalObjectContentSectionName(".jitlink_original_object_content");
 ArrayRef<const char *> ELFLinkGraphBuilderBase::DwarfSectionNames = DWSecNames;
 
 ELFLinkGraphBuilderBase::~ELFLinkGraphBuilderBase() = default;
